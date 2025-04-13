c29de2953ebcdb387b1828f8bbeb84c8b4543167
TanveerSinghRai <tsrai.2005@gmail.com>
2025-04-13 23:34:42 +0530

    Added Events

--- web.js
+++ web.js
@@ -1,6 +1,12 @@
-let drk = document.querySelector("#drk");
-let mode= light;
-const btn = ()=> {
-    console.log("dark.mode");
+let index = 0;
+const slides = document.querySelectorAll("#pic");
+
+function showSlides() {
+  slides.forEach(slide => slide.style.display = "none");
+  index++;
+  if (index > slides.length) index = 1;
+  slides[index - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
 }
-drk.addEventListener("click",btn);
+
+showSlides();
